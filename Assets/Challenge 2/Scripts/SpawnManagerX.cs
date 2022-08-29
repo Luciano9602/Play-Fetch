9da9e5d2ae4e04e8fa42535e1750a50579b0456e
Luciano9602 <mayluciano204@gmail.com>
2022-08-29 13:06:54 +0200

    First commit
    Adding comments to bugs fixed.

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -23,6 +23,7 @@ public class SpawnManagerX : MonoBehaviour
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        // Selects a random interger between 0 and the length of the ballPrefabs array.
         int selectedBall = Random.Range(0,ballPrefabs.Length);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
