b1ac8784afe0e36407db7edc094e626cbbbf2d1a
Luciano9602 <mayluciano204@gmail.com>
2022-08-29 13:21:27 +0200

    Second Commit
    Adding debug log to show game is over.

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -11,9 +11,14 @@ public class DestroyOutOfBoundsX : MonoBehaviour
     void Update()
     {
         // Destroy dogs if x position less than left limit or destroy balls if y position is less than bottomLimit
-        if (transform.position.x < leftLimit || transform.position.y < bottomLimit )
+        if (transform.position.x < leftLimit)
         {
             Destroy(gameObject);
-        } 
+
+        } else if(transform.position.y < bottomLimit)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
     }
 }
